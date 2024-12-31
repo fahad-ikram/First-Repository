16a99fc7f3bdee9addde57b6c8a7422e02e65c5a
Fahad Ikram <fahadikram118@gmail.com>
2024-12-31 14:26:32 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 # Initialize two variables
 a = 5
 b = 10
-print(f"Before swapping:\na: {a}\nb: {b}")
+print(f"Before swapping:\na :-> {a}\nb :-> {b}")
 # Swap using arithmetic operations
 a = a + b  # Step 1: a now holds the sum of a and b
 b = a - b  # Step 2: b now holds the original value of a
 a = a - b  # Step 3: a now holds the original value of b
-print(f"After swapping:\na: {a}\nb: {b}")
+print(f"After swapping:\na :-> {a}\nb :-> {b}")
